a69e7157df49ab11698c0e2e0b13568895776f85
Kendal Merrick <merrickks@gmail.com>
2016-09-03 21:19:28 -1000

    Added functionality to prototype functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Creates a matrix and defines helper functions
 makeCacheMatrix <- function(x = matrix()) {
-
+     invMatrix = NULL
+     set = function(y) {
+          x <<- y
+          invMatrix <<- NULL
+     }
+     get = function() x
+     setInverse = function(inverse) invMatrix <<- inverse 
+     getInverse = function() invMatrix
+     list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Calls makeCacheMatrix and checks if inverse has already been solved
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Calls the getter helper function of makeCacheMatrix 
+     invMatrix = x$getInverse()
+     ## Check if the cached matrix exists
+     if (!is.null(invMatrix)){
+          ## If found, retrieve cached matrix 
+          message("Cached matrix found!")
+          return(invMatrix)
+     }
+     ## If the cached matrix is null, solve the inverse
+     solveMatrix = x$get()
+     invMatrix = solve(solveMatrix, ...)
+     ## Call the setter function to store the inverse matrix in cache
+     x$setInverse(invMatrix)
+     return(invMatrix)
 }
